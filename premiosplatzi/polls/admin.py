e23804b88391e11fde81d88290e12e03f4c46f96
Atlantox <atlantox7@gmail.com>
2023-04-02 10:06:05 -0400

    course finished

--- premiosplatzi/polls/admin.py
+++ premiosplatzi/polls/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from . import models
 
+class ChoiceInline(admin.StackedInline):
+    model = models.Choice
+    extra = 4
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    search_fields = ['question_text']
+    list_display = ['id', 'question_text', 'pub_date', 'was_published_recently']
+    list_filter = ['pub_date']
+    inlines = [ChoiceInline]
+
 class ChoiceAdmin(admin.ModelAdmin):
     readonly_fields = ['votes']
 
-admin.site.register(models.Question)
+admin.site.register(models.Question, QuestionAdmin)
 admin.site.register(models.Choice, ChoiceAdmin)
